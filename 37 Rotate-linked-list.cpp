c3faf1972efc1f81e8ee1fd6449b0f83842737fa
yashul117 <yashultyagi11@gmail.com>
2022-03-03 18:16:35 +0530

    Linked List

--- 37 Rotate-linked-list.cpp
+++ 37 Rotate-linked-list.cpp
@@ -13,21 +13,38 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if (head == NULL || head->next == NULL || k == 0) return head;
 
+//         Naive Approach:
+//         while(k--) {
+//             ListNode* temp=head;
+//             while(temp->next->next) {
+//                 temp=temp->next;
+//             }
+//             ListNode* end=temp->next;
+
+//             temp->next=NULL;
+//             end->next=head;
+//             head=end;
+//         }
+//         return head;
+
         ListNode* temp = head;
-        int length = 1;
-        while (temp->next != NULL) {
-            ++length;
+        int len = 1;
+
+        while (temp->next) {
+            len = len + 1;
             temp = temp->next;
         }
-
         temp->next = head;
-        k = k % length;
-        int end = length - k;
-        while (end--) temp = temp->next;
+        k = k % len;
+        int cut = len - k;
 
-        head = temp->next;
-        temp->next = NULL;
+        for (int i = 1; i < cut; i++) {
+            head = head->next;
+        }
+        ListNode* newHead = head->next;
+        head->next = NULL;
 
-        return head;
+        return newHead;
+
     }
 };
