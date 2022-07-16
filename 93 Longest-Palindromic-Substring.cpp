af9bf54b438a0d67fbfcb80a7e7962f552b981ec
yashul117 <yashultyagi11@gmail.com>
2022-07-16 10:03:17 +0530

    Strings

---
+++ 93 Longest-Palindromic-Substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:    
+    string longestPalindrome(string s) {
+        int n=s.length(),best_len=0;
+        string best_str="";
+        for(int mid=0;mid<n;mid++) {
+            for(int x=0;mid-x>=0 && mid+x<n;x++) {
+                if(s[mid-x]!=s[mid+x]) {
+                    break;
+                }
+                
+                int len=2*x+1;
+                
+                if(len>best_len) {
+                    best_len=len;
+                    best_str=s.substr(mid-x,len);
+                }
+            }
+        }
+        
+        for(int mid=0;mid<n;mid++) {
+            for(int x=1;mid-x+1>=0 && mid+x<n;x++) {
+                if(s[mid-x+1]!=s[mid+x]) {
+                    break;
+                }
+                
+                int len=2*x;
+                
+                if(len>best_len) {
+                    best_len=len;
+                    best_str=s.substr(mid-x+1,len);
+                }
+            }
+        }
+        
+        return best_str;
+    }
+};
