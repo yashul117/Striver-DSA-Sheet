48d4d500e9af0078396263ba3b2f48bf0af68769
yashul117 <yashultyagi11@gmail.com>
2022-07-21 23:10:08 +0530

    Strings

--- 99 Minimum-characters-needed-to-be-inserted-in-the-beginning-to-makes-string-palindromic.cpp
+++ 99 Minimum-characters-needed-to-be-inserted-in-the-beginning-to-makes-string-palindromic.cpp
@@ -40,6 +40,33 @@ public:
         constructLPS(concat,lps);
         
         return str.length()-lps.back();
+
+
+        // Optimal Solution: 2-pointer approach
+/*        int n = str.length();
+	    int left = 0;
+	    int right = n - 1;
+	    int added = 0;
+	    while (left < right) {
+	        // if left and right characters are same increase
+	        // left and decrease right pointer
+	        if (str[left] == str[right]) {
+	            left++;
+	            right--;
+	        }
+	        // else think of adding some characters at front and
+	        // start comparing the elements again
+	        else {
+	            left = 0;
+	            added = n - right;
+	            if (str[left] == str[right]) {
+	                added--;
+	                left++;
+	            }
+	            right--;
+	        }
+	    }
+	    return added;*/
     }
 };
 
