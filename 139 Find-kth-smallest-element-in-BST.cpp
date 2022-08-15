63020e994ea42c96288d4601a7cfdc0b448a9bf7
yashul117 <yashultyagi11@gmail.com>
2022-08-15 10:59:39 +0530

    BST

---
+++ 139 Find-kth-smallest-element-in-BST.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void findInorder(TreeNode* root, vector<int> &inorder) {
+        if(root==NULL)
+            return;
+        
+        findInorder(root->left,inorder);
+        inorder.push_back(root->val);
+        findInorder(root->right,inorder);
+    }
+    
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> inorder;
+        findInorder(root,inorder);
+        
+        return inorder[k-1];
+    }
+};/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void findInorder(TreeNode* root, vector<int> &inorder) {
+        if(root==NULL)
+            return;
+        
+        findInorder(root->left,inorder);
+        inorder.push_back(root->val);
+        findInorder(root->right,inorder);
+    }
+    
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> inorder;
+        findInorder(root,inorder);
+        
+        return inorder[k-1];
+    }
+};
